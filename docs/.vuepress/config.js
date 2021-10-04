315ad6d0c63f8d76edacdf0938cd74837c625ead
Lee-Jp <lijiapeng@chunyu.me>
2021-10-04 21:50:10 +0800

    :sparkles:    : 添加链表刷题

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,10 @@
+/*
+ * @Description: 配置文件
+ * @Date: 2021-09-21 18:19:51
+ * @FilePath: /blog/docs/.vuepress/config.js
+ * @LastEditors: LiJiapeng
+ * @LastEditTime: 2021-10-04 21:48:21
+ */
 module.exports = {
   lang: 'zh-CN',
   title: '前端进阶积累',
@@ -23,23 +30,59 @@ module.exports = {
       //   ]
       // },
       { text: 'article', link: '/article/' },
+      { text: '算法', link: '/algorithm/' },
       { text: '关于', link: '/about/' }
     ],
     // 不同子路径下的页面会使用不同的侧边栏
-    sidebar: {
-      '/guide/': [
-        {
-          text: 'Guide',
-          children: ['/guide/README.md', '/guide/getting-started.md'],
-        },
-      ],
-      '/reference/': [
-        {
-          text: 'Reference',
-          children: ['/reference/cli.md', '/reference/config.md'],
-        },
-      ],
-    },
+    sidebar: [
+      {
+        text: 'algorithm',
+        link: '/algorithm/',
+        children: [
+          {
+            text: '链表',
+            link: '/链表/',
+            children: [
+              {
+                text: '141. 环形链表',
+                link: '/algorithm/链表/001-141. 环形链表.md',
+              },
+              {
+                text: '142. 环形链表 II',
+                link: '/algorithm/链表/002-142. 环形链表 II.md',
+              },
+              {
+                text: '202. 快乐数',
+                link: '/algorithm/链表/003-202. 快乐数.md',
+              },
+              {
+                text: '206. 反转链表',
+                link: '/algorithm/链表/004-206. 反转链表.md',
+              },
+              {
+                text: '92. 反转链表 II',
+                link: '/algorithm/链表/005-92. 反转链表 II.md',
+              },
+            ],
+          },
+        ],
+      },
+      // '/algorithm/': [
+        
+      //   '/algorithm/',
+      //   {
+      //     text: '链表',
+      //     children: ['/LeetCode刷题/链表/001-141. 环形链表.md', '/guide/getting-started.md'],
+      //   },
+      // ],
+      
+      // '/reference/': [
+      //   {
+      //     text: 'Reference',
+      //     children: ['/reference/cli.md', '/reference/config.md'],
+      //   },
+      // ],
+    ],
     sidebarDepth: 2,// sidebar深度
     editLink: false, // 编辑链接
     // editLinkText: '帮助我改善这个页面', // 链接字段
---
+++ docs/algorithm/README.md
@@ -0,0 +1,8 @@
+<!--
+ * @Description: 
+ * @Date: 2021-10-04 21:28:54
+ * @FilePath: /blog/docs/algorithm/ABOUT.md
+ * @Url: 
+ * @LastEditors: LiJiapeng
+ * @LastEditTime: 2021-10-04 21:28:56
+-->
